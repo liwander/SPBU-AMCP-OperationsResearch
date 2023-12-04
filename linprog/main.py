2d4897362097cc57150e7b66f4b9afd28e3b9d6f
Anver Gadzhiev <gadzhiev.anver@gmail.com>
2023-12-04 16:51:22 +0300

    simplex method visiualization was added

--- linprog/main.py
+++ linprog/main.py
@@ -1,7 +1,29 @@
 from simplex_method import simplex_method
+import numpy as np
+from intvalpy  import lineqs
+import matplotlib as plt
+from gilp.simplex import LP
+import gilp
 
+# c = [-8,-10, -7]
+# A = [[1, 3, 2], [1, 5, 3]]
+# b = [10, 8]
 
-c = [-8,-10, -7]
-A = [[1, 3, 2], [1, 5, 3]]
-b = [10, 8]
+c = np.array([5, 3])
+A = np.array([[2, 1],
+              [1, 1],
+              [1, 0]])
+b = np.array([20, 16, 7])
+
 print(simplex_method(c, A, b))
+
+b = np.transpose(b)
+c = np.transpose(c)
+
+lp = LP(A,b,c)
+# print(gilp.visualize.feasible_region(lp))
+gilp.visualize.simplex_visual(lp).show()
+
+
+# m = size
+# print(lineqs(-A, -b))
--- linprog/simplex_method.py
+++ linprog/simplex_method.py
@@ -1,6 +1,18 @@
 import numpy as np
-
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+from numpy.linalg import solve
+
+def current_optimal_solution(tableau):
+    optimal_solution = tableau[0, -1]
+    opt_arg = np.array([])
+    for j in range(tableau.shape[1] - tableau.shape[0]):
+        xpos = np.where(tableau[1:, j] == 1)[0]
+        if xpos.size == 0:
+            opt_arg = np.append(opt_arg, 0)
+        else:
+            opt_arg = np.append(opt_arg, tableau[int(xpos[0]) + 1, -1])
+    return optimal_solution, opt_arg
+        
 def simplex_method(c, A, b):
     c = np.array(c)
     A = np.array(A)
@@ -17,37 +29,35 @@ def simplex_method(c, A, b):
 
     sepline = '=' * 10 + '\n'
     print(tableau)
-    print(sepline)
-    maxiter = len(b)
+    # print(sepline)
+    maxiter = 10
     it = 0
+    res = []
+    res = current_optimal_solution(tableau)
+    print(res)
+
     # print(tableau[0, :-(1 + len(b))])
-    while np.any(tableau[0] > 0) and it < maxiter:
+    while np.any(tableau[0, 0:-1] < 0):
         it += 1
-        pivot_col = np.argmax(tableau[0, 0:])
+        pivot_col = np.argmin(tableau[0, 0:-1])
         indicator_col = tableau[1:, -1] / tableau[1:, pivot_col]
         pivot_row = np.where(indicator_col > 0, indicator_col, np.inf).argmin() + 1
-        print(f"iteration initial tableau: \n{tableau}")
-        print(f'indicator column: \n{indicator_col}')
+        # print(f"iteration initial tableau: \n{tableau}")
+        # print(f'indicator column: \n{indicator_col}')
 
         pivot_element = tableau[pivot_row, pivot_col]
         tableau[pivot_row, :] /= pivot_element
-        print(f"tableau after indenting pivot row: \n{tableau}")
+        # print(f"tableau after indenting pivot row: \n{tableau}")
 
 
         for i in range(m + 1):
             if i != pivot_row:
                 tableau[i, :] -= tableau[i, pivot_col] * tableau[pivot_row, :]
-                print(f"tableau after pivoting:  \n{tableau}")
+                # print(f"tableau after pivoting:  \n{tableau}")
 
-        print()
-    optimal_solution = tableau[0, -1]
-    opt_arg = np.array([])
-    for j in range(n):
-        xpos = np.where(tableau[1:, j] == 1)[0]
-        if xpos.size == 0:
-            opt_arg = np.append(opt_arg, 0)
-        else:
-            opt_arg = np.append(opt_arg, tableau[int(xpos[0]) + 1, -1])
+        # print()
+        # print(tableau)
+        res = current_optimal_solution(tableau)
+        # print(res)
 
-    return optimal_solution, opt_arg
-
+    return res
